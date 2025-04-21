e35728af75d5cbcacca737771aeabf2c785d1910
Nick Edwards <nickedwards.dev@gmail.com>
2025-04-20 18:13:52 -0700

    Dynamic load of dim_schmetzer_score_points table

--- data/etl/fbref_mls_player_misc_stats.py
+++ data/etl/fbref_mls_player_misc_stats.py
@@ -6,79 +6,16 @@ from dependencies.get_from_fbref import get_FBref_mls_player_misc_stats
 from data_handler import DataHandler
 data_handler = DataHandler()
 
-# TODO MOVE THESE TO JSON VARS
-# database_name = "mls_stats.db"
-# database_path = f"data/database/{database_name}"
-# # data/database/mls_stats.db
-# year=2018
-# FBref_current_url='https://FBref.com/en/comps/22/misc/Major-League-Soccer-Stats'
-# FBref_2024_url=f'https://FBref.com/en/comps/22/{year}/misc/{year}-Major-League-Soccer-Stats'
-# raw_table = 'raw_FBref_mls_players_all_stats_misc'
 
-# dim_schmetzer_score_points = {
-#   "aerial duels won": {
-#     "point_value": 1,
-#     "abbrev": "adw"
-#   },
-#   "aerial duels lost": {
-#     "point_value": -0.75,
-#     "abbrev": "adl"
-#   },
-#   "tackles won": {
-#     "point_value": 1,
-#     "abbrev": "tkwon"
-#   },
-#   "interceptions": {
-#     "point_value": 0.75,
-#     "abbrev": "inter"
-#   },
-#   "recoveries": {
-#     "point_value": 0.5,
-#     "abbrev": "recov"
-#   }
-# }
+### Insert into dim tables
+data_handler.insert_dim_schmetzer_score_points()
 
-# ### Insert into raw table
+### Insert into raw table
 data_handler.insert_historical_raw_FBref_mls_players_all_stats_misc()
 data_handler.insert_current_raw_FBref_mls_players_all_stats_misc()
-#####
-# conn = connect_db(database_name, database_path)
-# c = conn.cursor()
-# try:
-#     df = get_FBref_mls_player_misc_stats(year=year, url=FBref_2024_url)
-#     # Once new data obtained, remove existing data, then insert
-#     c.execute(f"DELETE FROM {raw_table} WHERE season = ?", (year,))
-#     print(f'Deleted from table: {raw_table} where season = {year}')
-#     conn.commit()
-#     df.to_sql(raw_table, conn, if_exists='append', index=False)
-#     print(f'Inserted into table: {raw_table} where season = {year}')
-#     conn.commit()
-# except sqlite3.Error as e:
-#     print(e)
-# finally:
-#     conn.close()
-#####
 
 ### Transform raw data for staging table
 data_handler.insert_stg_FBref_mls_players_all_stats_misc()
-#####
-# conn = connect_db(database_name, database_path)
-# c = conn.cursor()
-# try:
-#     # for sql_file in glob.glob('data/etl/sql/transform/*.sql'):
-#     sql_file = glob.glob('data/etl/sql/transform/load_stg_FBref_mls_players_all_stats_misc.sql')
-#     with open(sql_file, 'r') as f:
-#         table_name = os.path.splitext(os.path.basename(sql_file))[0].replace('load_', '')
-#         sql = f.read()
-#         c.executescript(sql)
-#         # c.executescript(sql, {'point_value': dim_schmetzer_score_points['point_value'], 'description': dim_schmetzer_score_points['abbrev']})
-#         print(f'Inserted into table: {table_name} where season = {year} by player')
-#         conn.commit()
-# except sqlite3.Error as e:
-#     print(e)
-# finally:
-#     conn.close()
-#####
 
 
 
