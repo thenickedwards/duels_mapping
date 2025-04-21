4d44e3c0bb08b86a725335fa07c10c6dd3754ef8
Nick Edwards <nickedwards.dev@gmail.com>
2025-04-21 12:51:26 -0700

    Creating tables for player Schmetzer Scores and inserting data

--- data/etl/fbref_mls_player_misc_stats.py
+++ data/etl/fbref_mls_player_misc_stats.py
@@ -2,16 +2,20 @@ from data_handler import DataHandler
 data_handler = DataHandler()
 
 
-### Insert into dim tables
-data_handler.insert_dim_schmetzer_score_points()
+def pipeline_FBref_misc_stats_to_schmetzer_scores_players():    
+    ### Insert into dim tables
+    data_handler.insert_dim_schmetzer_score_points()
 
-### Insert into raw table
-data_handler.insert_historical_raw_FBref_mls_players_all_stats_misc()
-data_handler.insert_current_raw_FBref_mls_players_all_stats_misc()
+    ### Insert into raw table
+    data_handler.insert_historical_raw_FBref_mls_players_all_stats_misc()
+    data_handler.insert_current_raw_FBref_mls_players_all_stats_misc()
 
-### Transform raw data for staging table
-data_handler.insert_stg_FBref_mls_players_all_stats_misc()
+    ### Transform raw data for staging table
+    data_handler.insert_stg_FBref_mls_players_all_stats_misc()
+    
+    ### Create and Insert into schmetzer_scores_players calculated points and scores (each table per season)
+    data_handler.insert_schmetzer_scores_players()
 
 
-
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    pipeline_FBref_misc_stats_to_schmetzer_scores_players()
