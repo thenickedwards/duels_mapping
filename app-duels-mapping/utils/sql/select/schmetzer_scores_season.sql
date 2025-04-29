fad05600533664c277cfc32df1d182e26c2128d5
Nick Edwards <nickedwards.dev@gmail.com>
2025-04-29 13:01:33 -0700

    Abstracting away SQL to utils/sql

---
+++ app-duels-mapping/utils/sql/select/schmetzer_scores_season.sql
@@ -0,0 +1,25 @@
+SELECT 
+    player_name,
+    player_nationality,
+    position,
+    squad,
+    player_age,
+    player_yob,
+    nineties,
+    schmetzer_score,
+    schmetzer_rk,
+    aerial_duels_won,
+    aerial_duels_won_pts,
+    aerial_duels_lost,
+    aerial_duels_lost_pts,
+    aerial_duels_total,
+    aerial_duels_total_pts,
+    aerial_duels_won_pct,
+    tackles_won,
+    tackles_won_pts,
+    interceptions,
+    interceptions_pts,
+    recoveries,
+    recoveries_pts
+FROM "schmetzer_scores_{year}"
+ORDER BY schmetzer_score DESC;
