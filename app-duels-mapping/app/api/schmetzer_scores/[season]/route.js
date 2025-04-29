f3d4ec7d3a9b9141126e80edcee3e15c89f11ded
Nick Edwards <nickedwards.dev@gmail.com>
2025-04-28 19:38:41 -0700

    Current and Season routing functional

---
+++ app-duels-mapping/app/api/schmetzer_scores/[season]/route.js
@@ -0,0 +1,44 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import fs from "fs";
+import path from "path";
+
+let db = null;
+
+async function getDatabasePath() {
+  const configPath = path.join(process.cwd(), "..", "data", "data_vars.json");
+  const configData = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+  const databaseName = configData.database.name;
+  const databasePathTemplate = configData.database.path;
+  const resolvedDatabasePath = path.join(
+    "..",
+    databasePathTemplate.replace("_DATABASE_NAME_", databaseName)
+  );
+  return resolvedDatabasePath;
+}
+
+export async function GET(req, { params }) {
+  const { season } = params;
+
+  if (!db) {
+    const dbPath = await getDatabasePath();
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+  }
+
+  try {
+    const scores = await db.all(`SELECT * FROM schmetzer_scores_${season}`);
+    return new Response(JSON.stringify(scores), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Database query error:", error);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      headers: { "Content-Type": "application/json" },
+      status: 500,
+    });
+  }
+}
