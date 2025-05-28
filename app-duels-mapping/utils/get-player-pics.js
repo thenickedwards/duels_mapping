412894fef9f4918b0c5bae35565243f2b2684012
Nick Edwards <nickedwards.dev@gmail.com>
2025-05-28 16:15:42 -0700

    Fine tuning helper function

--- app-duels-mapping/utils/get-player-pics.js
+++ app-duels-mapping/utils/get-player-pics.js
@@ -4,11 +4,12 @@ function removeAccents(str) {
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
-export async function getPlayerPic(playerName, verbose = 2) {
+export async function getPlayerPic(playerName, verbose = 1) {
   const playerNameSimple = removeAccents(playerName);
   const playerNameUrl = playerNameSimple.toLowerCase().replace(/\s/g, "-");
   const playerUrl = `https://www.mlssoccer.com/players/${playerNameUrl}/`;
-  if (verbose >= 2) console.log(`Getting ${playerName}`, playerUrl);
+  if (verbose >= 1) console.log(`Getting player headshot of ${playerName}...`);
+  if (verbose >= 2) console.log(`Sourcing picture from ${playerUrl}`);
 
   const $ = await cheerio.fromURL(testUrl);
 
