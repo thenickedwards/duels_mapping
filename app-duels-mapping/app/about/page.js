b4e009dcd565b56430f0f568ff8dba0c305912b9
Nick Edwards <nickedwards.dev@gmail.com>
2025-06-05 15:04:39 -0700

    Adding Methods text

--- app-duels-mapping/app/about/page.js
+++ app-duels-mapping/app/about/page.js
@@ -6,28 +6,30 @@ export default function AboutPage() {
       <h1>About Us</h1>
       <p>
         This project began as a thought experiment and a dream of working in
-        professional sports data and analytics. In many press conferences Coach
-        Schmetzer will tap his pen on the table and reference his preferred
-        statistic: <span style={{ fontStyle: "italic" }}>duels won</span>. If
-        working for the Sounders&apos; data and analytics team, how could we
-        deliver a fuller picture to the coaching staff of how possession is won
-        or lost by a particular player. Understanding when a player is more
-        likely to gain or lose possession, can help the team address
-        shortcomings in training as well as strategize tactics and matchups in
-        upcoming games.
+        professional sports data and analytics. At many of his post-game press
+        conferences, Coach Schmetzer will tap his pen on the table and reference
+        his preferred statistic:{" "}
+        <span style={{ fontStyle: "italic" }}>duels won</span>. If working for
+        the Sounders&apos; data and analytics team, how could we deliver a
+        fuller picture to the coaching staff of how possession is won or lost by
+        a particular player. Understanding when a player is more likely to gain
+        or lose possession, can inform tactical decisions, highlight areas for
+        individual improvement, and shape game strategy for opponents and
+        matchups.
       </p>
       <p>
-        To achieve this goal, we created a{" "}
+        To achieve this goal, we developed a{" "}
         <span style={{ fontStyle: "italic" }}>Contested Possession Metric</span>{" "}
-        -- a composite statistic weighted by aerial duels won, aerial duels
-        lost, tackles won, interceptions, and recoveries using a custom
-        algorithm. Since “Contested Possession Metric” is a bit of a mouthful,
+        -- a composite statistic using a custom algorithm to weight aerial duels
+        won and lost, tackles won, interceptions, and recoveries in order to
+        reflect the in-game contribution of creating turnovers or securing
+        possession. Since “Contested Possession Metric” is a bit of a mouthful,
         we&apos;ve dubbed it the <strong>Schmetzer Score</strong>.
       </p>
       <p>
         <Link href="https://thenickedwards.github.io/portfolio/">
           <strong>Nick Edwards</strong>
-        </Link>{" "}
+        </Link>
         architected the data environment, developed the ETL pipelines which
         ingest the data, designed the algorithm to calculate the scores, and
         built the API routing which delivers hard numbers to the front end.
@@ -37,9 +39,16 @@ export default function AboutPage() {
           <strong>Juanita Samborski</strong>
         </Link>
         brought all the stats to life in a vibrant and interactive Next.js
-        dashboard that leverages an intuitive UX/UI -- centering the user and
-        offering a seamless digital experience.
+        dashboard that leverages an intuitive UX/UI, centers the user, and
+        offers a seamless experience.
       </p>
+      <Link href="./methods">
+        Read more about the thought process supporting our approach and how the
+        statistics were weighted on the Methods page.
+      </Link>
+      <Link href="./team">
+        Meet the two developers behind this project on the Team page.
+      </Link>
     </main>
   );
 }
