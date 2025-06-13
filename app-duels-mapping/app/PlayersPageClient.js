9bd7ec585173df6ab9a132c7b3058e9e0d0c8af3
Juanita Samborski <juanita@breakthroughwebsite.com>
2025-06-13 22:02:07 +0300

    Resolved build error.

---
+++ app-duels-mapping/app/PlayersPageClient.js
@@ -0,0 +1,9 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import PlayersPageCore from './PlayersPageCore';
+
+export default function PlayersPageClient() {
+  const searchParams = useSearchParams();
+  return <PlayersPageCore searchParams={searchParams} />;
+}
