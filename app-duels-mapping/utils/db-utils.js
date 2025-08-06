2cb8f08d6bc47b0e3ec5fe738f2d28480c0d5a0b
Nick Edwards <nickedwards.dev@gmail.com>
2025-08-06 13:11:23 -0700

    Including node and other changes

--- app-duels-mapping/utils/db-utils.js
+++ app-duels-mapping/utils/db-utils.js
@@ -28,7 +28,7 @@ export async function getDatabasePath(verbose = 1) {
   if (!fs.existsSync(absolutePath) && verbose >= 1) {
     throw new Error(`Database file does not exist at ${absolutePath}`);
   }
-  if (verbose >= 1) console.log("Found DB at:", relativePath);
+  if (verbose >= 2) console.log("Found DB at:", relativePath);
   if (verbose >= 2) console.log("Resolved DB absolute path:", absolutePath);
 
   return absolutePath;
@@ -40,38 +40,4 @@ export function getSqlSelect(sqlFile) {
   const sqlPath = path.join(__dirname, "sql", "select", sqlFile); // relative to db-utils.js
   const sql = fs.readFileSync(sqlPath, "utf-8");
   return sql;
-}
-
-// // Check if running locally, use SQLite db, else use the Supbase db  //
-
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import { createClient } from "@supabase/supabase-js";
-
-export async function getDbClient(origin) {
-  let sqliteDb = null;
-  const isLocal = origin.includes("localhost") || origin.includes("127.0.0.1");
-  // if origin localhost or 127.0.0.1, use SQLite db
-  if (isLocal) {
-    if (!sqliteDb) {
-      const dbPath = await getDatabasePath();
-      sqliteDb = await open({
-        filename: dbPath,
-        driver: sqlite3.Database,
-      });
-    }
-    return {
-      type: "sqlite",
-      client: sqliteDb,
-    };
-  }
-  // Supabase connection
-  const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
-  );
-  return {
-    type: "supabase",
-    client: supabase,
-  };
 }
